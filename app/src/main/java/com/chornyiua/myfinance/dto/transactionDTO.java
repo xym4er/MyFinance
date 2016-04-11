4daf99f22f6bf43e416921daf56e0c7f27b08b29
xym4er <xym4er@gmail.com>
2016-04-11 16:57:20 +0300

    1

---
+++ app/src/main/java/com/chornyiua/myfinance/dto/transactionDTO.java
@@ -0,0 +1,16 @@
+package com.chornyiua.myfinance.dto;
+
+/**
+ * Created by ChornyiUA on 11.04.2016.
+ */
+public class transactionDTO {
+    private int ID;
+    private int cashBefore;
+    private int cashAfter;
+    private int value;
+    private long date;
+    private String title;
+    private String comment;
+
+
+}
